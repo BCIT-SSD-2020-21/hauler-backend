a1251c524cf61ae53004cb4af832fd0bd6e050d8
mandeep1580 <mandeep15@gmail.com>
2021-04-24 03:51:45 -0700

    updated the users action schema for post response #2

--- models/userAction.js
+++ models/userAction.js
@@ -2,12 +2,16 @@ const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema;
 
+const UserResponseSchema = new Schema ({
+    userResponse: { type: Boolean, required: true},
+    userCounterOfferPrice: { type: Number, required: true},
+})
+
 const UserActionSchema = new Schema ({
     serviceProviderId: {type: String, required: true},
     postId: {type: String, required: true},
-    userAction: { type: String, required: true},
-    actionPrice: { type: Number, required: true},
-    userCounterOfferPrice: { type: Number, required: true},
+    userResponse: [UserResponseSchema],
 });
+
 
 module.exports = mongoose.model('UserAction', UserActionSchema);
