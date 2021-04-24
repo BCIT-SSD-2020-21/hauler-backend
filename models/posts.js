0b022807cc613cf8338984780ea73dbb61c1b99c
mandeep1580 <mandeep15@gmail.com>
2021-04-24 03:49:18 -0700

    added the post schema for the user #2

--- models/posts.js
+++ models/posts.js
@@ -10,31 +10,44 @@ const postSchema = new Schema([
     {
         userId: { type: String, required: true },
         service: { type: String, required: true },
-        itemName: { type: String, required: true },
-        width: { type: String },
-        height: { type: String },
-        weight: { type: String },
-        qty: { type: Number, required: true, default: 1 },
-        images: [imageSchema],
-        pickUpProvince: { type: String, required: true },
-        pickUpCity: { type: String, required: true },
-        pickUpStreetAddress: { type: String, required: true },
-        pickUpZipCode: { type: String, required: true },
-        pickUpContactPerson: { type: String, required: true },
-        pickUpContactNumber: { type: String, required: true },
-        pickUpSpecialInstruction: { type: String, required: true },
-        dropOffProvince: { type: String, required: true },
-        dropOffCity: { type: String, required: true },
-        dropOffStreetAddress: { type: String, required: true },
-        dropOffZipCode: { type: String, required: true },
-        dropOffContactPerson: { type: String, required: true },
-        dropOffContactNumber: { type: String, required: true },
-        dropOffSpecialInstruction: { type: String, required: true },
+        postHeading: { type: String },
+        postDescription: {type: String},
+        loadWeight: {type: String},
+        numberOfItems: {type: String},
+        loadImages: [imageSchema],
         price: { type: Number, required: true, default: 50 },
-        totalOffers: { type: Number, default: 0 },
+        totalOffers: { type: Number, default: 0 }, //----confirm applicability?
         show: { type: Boolean, default: true },
         status: { type: String, required: true },
     }
 ]);
 
+const pickSchema = new Schema([
+    {
+        contactPerson: { type: String, required: true },
+        contactNumber: { type: String, required: true },
+        province: { type: String, required: true },
+        city: { type: String, required: true },
+        streetAddress: { type: String, required: true },
+        unitNumber: { type: String},
+        zipCode: { type: String, required: true },
+        specialInstruction: { type: String, required: true }
+    }
+]);
+
+const dropSchema = new Schema([
+    {
+        contactPerson: { type: String, required: true },
+        contactNumber: { type: String, required: true },
+        province: { type: String, required: true },
+        city: { type: String, required: true },
+        streetAddress: { type: String, required: true },
+        unitNumber: { type: String},
+        zipCode: { type: String, required: true },
+        specialInstruction: { type: String, required: true }
+    }
+]);
+
 module.exports = mongoose.model('Post',postSchema);
+module.exports = mongoose.model('Pick up',pickSchema);
+module.exports = mongoose.model('Drop off',dropSchema);
