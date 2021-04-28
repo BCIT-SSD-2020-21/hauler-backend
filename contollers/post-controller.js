ee1032b0c4029a9ecd2b7dba0b83d3d2b72e2780
mandeep1580 <mandeep15@gmail.com>
2021-04-28 14:02:53 -0700

    Added the post and get method for posts model

--- contollers/post-controller.js
+++ contollers/post-controller.js
@@ -1,5 +1,25 @@
-const getPosts = async (req, res, next) => {
-   await res.send('This works!');
+const PostData = require('../models/posts.js')
+
+const getPosts = async (req, res) => {
+   try {
+        const posts = await PostData.find();
+        res.status(200).json(posts)
+   } catch (error) {
+    res.status(404).json({ message: error.message });
+   }
 };
 
+const createPost = async (req, res) => {
+    const postMessage = req.body;
+    const newPost = new PostData(postMessage);
+
+    try {
+            await newPost.save()
+            res.status(201).json(newPost);
+        } catch (error) {
+            res.status(409).json({ message: error.message });
+    }
+}
+
 exports.getPosts = getPosts;
+exports.createPost = createPost;
