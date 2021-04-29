e6b42244adc0a502df396f778b695bb1160e713b
mandeep1580 <mandeep15@gmail.com>
2021-04-29 15:15:05 -0700

    added comments and cleaned the file

--- server.js
+++ server.js
@@ -3,27 +3,32 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-const postRoutes = require('./routes/post-routes.js');
-const serviceProvidersRoutes = require('./routes/serviceProvider-router.js');
-
-
 require('dotenv').config();
-
 const app = express();
 const port = process.env.PORT || 5000;
 
+//importing routes
+const postRoutes = require('./routes/post-routes.js');
+const serviceProvidersRoutes = require('./routes/serviceProvider-router.js');
+
+//importing middleware
 app.use(cors());
-// app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 
+
+//defining restApi's
 app.use('/api/posts', postRoutes);
 app.use('/api/service-providers', serviceProvidersRoutes);
 
+
+//console logging port connection
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 })
 
+
+//connecting to MongoAtlas
 const uri = process.env.MONGO_URI;
 
 mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
