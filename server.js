33c7598c9e4cb53b4fb6a27231ba1041a8d50eac
mandeep1580 <mandeep15@gmail.com>
2021-04-29 11:30:33 -0700

    upated the postioning of routes to fix bodyparser error

--- server.js
+++ server.js
@@ -10,13 +10,12 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use('/posts', postRoutes);
-
 app.use(cors());
-app.use(express.json());
-app.use(bodyParser.json({limit: "30mb", extended: true}));
+// app.use(express.json());
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 
+app.use('/api/posts', postRoutes);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
