c05ab5d84e88161faa46be3b0ac59c28de9fc353
KulveerBrar <brarkulveer15@gmail.com>
2021-05-02 20:44:52 -0700

    Add service provider routes #10

--- routes/serviceProvider-router.js
+++ routes/serviceProvider-router.js
@@ -1,12 +1,12 @@
 const express = require('express');
-
 const serviceProviderController = require('../contollers/serviceProvider-controller')
-
 const router = express.Router();
 
-
-router.get('/', serviceProviderController.getServiceProvider );
-router.post('/', serviceProviderController.createServiceProvider );
+router.get('/', serviceProviderController.getServiceProvider);
+router.get('/:uid', serviceProviderController.getOneServiceProvider);
+router.post('/', serviceProviderController.createServiceProvider);
+router.delete('/:uid', serviceProviderController.deleteOneServiceProvider);
+router.post('/:uid', serviceProviderController.updateOneServiceProvider)
 
 module.exports = router;
 
