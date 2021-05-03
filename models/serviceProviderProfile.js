b5ea997beb838fd7092ddb8618811e7970a4e50b
KulveerBrar <brarkulveer15@gmail.com>
2021-05-02 23:54:03 -0700

    service provider schema updated #2

--- models/serviceProviderProfile.js
+++ models/serviceProviderProfile.js
@@ -2,34 +2,40 @@ const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema;
 
-const serviceLocationSchema = new Schema ({
-    serviceLocation: {type: String},
-    locationStatus: {type: Boolean}, 
+const serviceLocationSchema = new Schema({
+    serviceLocation: { type: String },
+    locationStatus: { type: Boolean },
 })
 
-const servicesProvidedSchema = new Schema ({
-    serviceProvided: {type: String},
-    serviceStatus: {type: Boolean},
+const servicesProvidedSchema = new Schema({
+    serviceProvided: { type: String },
+    serviceStatus: { type: Boolean },
     serviceLocations: [serviceLocationSchema]
 })
 
-const serviceProviderProfileSchema = new Schema ({
-    firstName: {type: String},
-    lastName: {type: String},
-    profilePicUrl: {type: String},
-    dateOfBirth: {type: Date},
-    province: {type: String},
-    city: {type: String},
-    streetAddress: {type: String },
-    unitNumber: {type: String},
-    email: {type: String},
-    contactNumber: {type: Number},
-    chequeDepositFormUrl: {type: String},
-    vehicleType: {type: String},
-    driverLicenseUrl: {type: String},
-    driverLicenseExpiry: {type: Date},
-    driverAbstractUrl: {type: String},
-    profileStatus: {type: Boolean},
+const vehicleTypeSchema = new Schema({
+    vehicle: { type: String }
+})
+
+const serviceProviderProfileSchema = new Schema({
+    uid: { type: String },
+    firstName: { type: String },
+    lastName: { type: String },
+    profilePicUrl: { type: String },
+    dateOfBirth: { type: Date },
+    timeStamp: { type: Date, required: true, default: Date.now },
+    province: { type: String },
+    city: { type: String },
+    streetAddress: { type: String },
+    unitNumber: { type: String },
+    email: { type: String },
+    contactNumber: { type: String },
+    chequeDepositFormUrl: { type: String },
+    vehicleType: [vehicleTypeSchema],
+    driverLicenseUrl: { type: String },
+    driverLicenseExpiry: { type: Date, default: Date.now },
+    driverAbstractUrl: { type: String },
+    profileStatus: { type: Boolean },
     serviceProvided: [servicesProvidedSchema]
 })
 
