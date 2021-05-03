56547fdbf29014540e5e58709a63fd69e10fb0b1
KulveerBrar <brarkulveer15@gmail.com>
2021-05-03 00:51:04 -0700

    user controller updated #11

--- contollers/user-controller.js
+++ contollers/user-controller.js
@@ -1,54 +1,104 @@
 const UserData = require('../models/userProfile.js')
 
-
-const getUser = async (req, res) => {
-   try {
-        const users = await UserData.find();
-        res.status(200).json(users)
-   } catch (error) {
-    res.status(404).json({ message: error.message });
-   }
-};
-
+//================================== To register new user =========================================//
 const createUser = async (req, res) => {
-    const { 
-        firstName,
-        lastName,
-        profilePicUrl,
-        dateOfBirth,
-        province,
-        city,
-        streetAddress,
-        unitNumber,
-        email,
-        contactNumber,
-        creditCardNumber,
-        expiryDate,
-        cvv,
-        profileStatus } = req.body;
+    try {
+        const {
+            firstName,
+            lastName,
+            profilePicUrl,
+            dateOfBirth,
+            province,
+            city,
+            streetAddress,
+            unitNumber,
+            email,
+            contactNumber,
+            creditCardNumber,
+            expiryDate,
+            cvv,
+            profileStatus } = req.body;
 
-    const newUser = new UserData({
-        firstName,
-        lastName,
-        profilePicUrl,
-        dateOfBirth,
-        province,
-        city,
-        streetAddress,
-        unitNumber,
-        email,
-        contactNumber,
-        creditCardNumber,
-        expiryDate,
-        cvv,
-        profileStatus });
+        const newUser = new UserData({
+            firstName,
+            lastName,
+            profilePicUrl,
+            dateOfBirth,
+            province,
+            city,
+            streetAddress,
+            unitNumber,
+            email,
+            contactNumber,
+            creditCardNumber,
+            expiryDate,
+            cvv,
+            profileStatus
+        });
 
         await newUser.save();
-        res.status(201).json({userProfile: newUser});
-  
+        res.status(201).json({ userProfile: newUser });
+    } catch (error) {
+        res.status(404).json({ message: error.message });
     }
+}
 
+//==================================== Get All users ================================================//
+const getUser = async (req, res) => {
+    try {
+        const users = await UserData.find();
+        res.status(200).json(users)
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+};
 
+//===================================== Get One user ================================================//
+const getOneUser = async (req, res) => {
+    try {
+        const id = req.params.uid;
+        let user = await UserData.findOne({ _id: id });
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+};
+
+//====================================== Delete One User =============================================//
+const deleteOneUser = async (req, res) => {
+    try {
+        const id = req.params.uid;
+        await UserData.deleteOne({ _id: id });
+        res.status(200).json("user deleted")
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+};
+
+//=============================== Edit user Profile =================================================//
+const updateOneUser = async (req, res) => {
+    try {
+        const id = req.params.uid;
+        const {
+            firstName,
+            lastName,
+            dateOfBirth,
+            province,
+            city,
+            streetAddress,
+            unitNumber,
+            contactNumber
+        } = req.body;
+        await UserData.findOneAndUpdate({ _id: id }, { $set: { firstName: firstName, lastName: lastName, dateOfBirth: dateOfBirth, province: province, city: city, streetAddress: streetAddress, unitNumber: unitNumber, contactNumber: contactNumber } });
+        res.status(200).json("User Info updated")
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+};
+
+exports.getOneUser = getOneUser;
 exports.getUser = getUser;
 exports.createUser = createUser;
+exports.deleteOneUser = deleteOneUser;
+exports.updateOneUser = updateOneUser;
 
