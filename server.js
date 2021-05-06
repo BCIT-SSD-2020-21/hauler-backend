d15eb954132007d47cbb39b4e00c2e426a213d81
KulveerBrar <brarkulveer15@gmail.com>
2021-05-05 22:15:55 -0700

    delete unwanted files, clean code #23

--- server.js
+++ server.js
@@ -5,13 +5,12 @@ const bodyParser = require("body-parser");
 
 require('dotenv').config();
 const app = express();
-const port =  3000;
+const port = 3000;
 
 //importing routes
 const postRoutes = require('./routes/post-routes.js');
 const serviceProvidersRoutes = require('./routes/serviceProvider-router.js');
 const userRoutes = require('./routes/user-router.js');
-const responseRoutes = require('./routes/response-routes.js')
 
 //importing middleware
 app.use(bodyParser.json());
@@ -22,8 +21,6 @@ app.use(cors());
 app.use('/api/posts', postRoutes);
 app.use('/api/service-providers', serviceProvidersRoutes);
 app.use('/api/users', userRoutes);
-app.use('/api/response', responseRoutes);
-
 
 //connecting to MongoAtlas
 const uri = process.env.MONGO_URI;
@@ -33,9 +30,9 @@ mongoose
     .then(() => {
         app.set("port", port);
         app.listen(port, () =>
-        console.log(`App running successfully on port http://localhost:${port}`)
+            console.log(`App running successfully on port http://localhost:${port}`)
         );
     })
     .catch(err => {
         console.log(err)
-});
+    });
