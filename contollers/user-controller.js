0122d5216e10ea5f22fa0e694975151cc3366ea8
KulveerBrar <brarkulveer15@gmail.com>
2021-05-14 16:23:25 -0700

    UserProfile update with cardholdername and status #35

--- contollers/user-controller.js
+++ contollers/user-controller.js
@@ -15,6 +15,7 @@ const createUser = async (req, res) => {
             unitNumber,
             email,
             contactNumber,
+            cardHolderName,
             creditCardNumber,
             expiryDate,
             cvv 
@@ -32,6 +33,7 @@ const createUser = async (req, res) => {
             unitNumber,
             email,
             contactNumber,
+            cardHolderName,
             creditCardNumber,
             expiryDate,
             cvv
